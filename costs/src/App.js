4b2c1be87b83c622aca4abbece54bbcf796753b2
Rafael Pereira <94644258+rafaelpssilva@users.noreply.github.com>
2022-03-19 14:11:32 -0300

    Criação dos components

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,8 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Home from './components/pages/Home.jsx'
+import Company from './components/pages/Company.jsx'
+import Contact from './components/pages/Contact.jsx'
+import NewProject from './components/pages/NewProject.jsx'
+
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <ul>
+        <li>Home</li>
+        <li>Contat</li>
+      </ul>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/company" element={<Company />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/newproject" element={<NewProject />} />
+      </Routes>
+      <footer>Footer</footer>
+    </Router>
   );
 }
 
